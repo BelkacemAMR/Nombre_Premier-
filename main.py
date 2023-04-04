291568537941267375ec454cab2ac5a3ad1206cd
Belkacem <abel-and-co@hotmail.com>
2023-04-04 11:40:11 +0200

    Version finale + Ajout exemples de tests

--- main.py
+++ main.py
@@ -6,3 +6,20 @@ def is_prime(n):
       if (n%i) == 0 or n == 1:
         return False
     return True
+
+# Tester plusieurs nombres différents
+
+print(is_prime(5))   # True
+print(is_prime(10))  # False
+print(is_prime(13))  # True
+print(is_prime(15))  # False
+print(is_prime(29))  # True
+print(is_prime(30))  # False
+
+# Tester les valeurs limites
+
+print(is_prime(1))   # False
+print(is_prime(2))   # True
+print(is_prime(3))   # True
+print(is_prime(4))   # False
+print(is_prime(2147483647))   # True, le plus grand nombre premier possible pour les entiers signés de 32 bits
